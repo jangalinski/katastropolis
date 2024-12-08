f562e01dc9afca4d4df97787eb0c166632b709d8
Jan Galinski <jan.galinski@holisticon.de>
2024-12-08 13:06:56 +0100

    solve 8

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,18 @@ plugins {
 dependencies {
   implementation(libs.arrow.core)
   implementation(libs.arrow.fx.coroutines)
+
+  implementation(libs.flexmark.html2md.converter)
+
   implementation(libs.krid)
   implementation(libs.kotlinx.benchmark.runtime)
 
   implementation(libs.aoc.kotlin)
+
+  testImplementation(platform("org.junit:junit-bom:5.11.3"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
+  testImplementation("org.assertj:assertj-core:3.26.3")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 allOpen {
@@ -43,3 +51,9 @@ kotlin {
   jvmToolchain(17)
 }
 
+tasks.test {
+  useJUnitPlatform()
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
+}
