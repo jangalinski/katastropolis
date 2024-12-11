defa802f31a8dd54cc8d5603485f7a264c23b82a
Jan Galinski <jan.galinski@holisticon.de>
2024-12-12 00:13:51 +0100

    day 4 gold

--- advent-of-code/src/main/kotlin/_2024/day04.kt
+++ advent-of-code/src/main/kotlin/_2024/day04.kt
@@ -2,81 +2,71 @@ package io.github.jangalinski.kata.aoc._2024
 
 import io.github.jangalinski.kata.aoc.AocUtil.Input
 import io.github.jangalinski.kata.aoc.AocUtil.KridExt.take
+import io.github.jangalinski.kata.aoc.Chrid
 import io.toolisticon.lib.krid.Krid
 import io.toolisticon.lib.krid.Krids
+import io.toolisticon.lib.krid.Krids.cell
+import io.toolisticon.lib.krid.Krids.krid
+import io.toolisticon.lib.krid.ascii
 import io.toolisticon.lib.krid.get
 import io.toolisticon.lib.krid.getValue
 import io.toolisticon.lib.krid.model.Cell
+import io.toolisticon.lib.krid.model.CellValue
+import io.toolisticon.lib.krid.model.adjacent
 import io.toolisticon.lib.krid.model.step.Direction
-
 
 fun main() {
 
-  fun read(test: Boolean = false, empty: Char = '.'): Krid<Char> {
-    val rows = Input(year = 2024, day = 4, part = 1, test = test)
-      .nonEmptyLines.map { it.toList().map { it -> if (it == 'X') '.' else it } }
-    return Krids.krid(rows, empty)
+  fun read(test: Boolean = false, empty: Char = '.'): Chrid {
+    val content = when(test) {
+      true -> """
+        MMMSXXMASM
+        MSAMXMSMSA
+        AMXSXMAAMM
+        MSAMASMSMX
+        XMASAMXAMM
+        XXAMMXXAMA
+        SMSMSASXSS
+        SAXAMASAAA
+        MAMMMXMMMM
+        MXMXAXMASX
+      """.trimIndent()
+      false -> Input(year = 2024, day = 4, part = 1, test = false).contentRaw
+    }
+    return Krids.krid(content, empty)
   }
 
   fun Cell.take(direction: Direction, count: Int): List<Cell> = this.take(direction, count, true)
 
   // find all 4-size beams that build the word XMAS
-  fun silver(test: Boolean = false): Int {
-    val krid = read(test)
-
-    return krid.cells().flatMap() { cell ->
+  fun silver(k: Chrid): Int {
+    return k.cells().flatMap() { cell ->
       Direction.entries.filterNot { Direction.NONE == it }
         .map { direction -> cell.take(direction, 4) }
-        .filter { krid.dimension.filterInBounds(it).size == 4 }
-    }.map { cells -> krid[cells].map { it.value }.joinToString("") }
+        .filter { k.dimension.filterInBounds(it).size == 4 }
+    }.map { cells -> k[cells].map { it.value }.joinToString("") }
       .count { "XMAS" == it }
   }
 
   // find all crosses of MAS, so the A is shared and opposite adjacent have values M,S
-  fun gold(test: Boolean = false): Int {
-    val krid = read(test)
+  fun gold(_k:Chrid): Int {
+    val k = krid(_k.ascii{ if ('X' == it) '.' else it })
 
-    val aas = krid.cellValues().filter { it.value == 'A' }
-      .map { value ->
-        value to value.cell.adjacent.filter { cell -> krid.dimension.isInBounds(cell) }.map { krid.getValue(it) }
-      }
-      .filter { it.second.size == 8 }
+    return k.cellValues().filter { 'A' == it.value }
+      .map { it to it.cell.adjacent(Direction.UP_LEFT, Direction.UP_RIGHT, Direction.DOWN_RIGHT, Direction.DOWN_LEFT)
+        .filter { k.dimension.isInBounds(it) }
+        .map { k.getValue(it) }
+      }.filter { it.second.size == 4 }
       .map {
-        it.second.map { it.value }.map { ch ->
-          when (ch) {
-            'M' -> true
-            'S' -> false
-            else -> null
-          }
-        }
-      }
-      .map {
-        listOf(
-          it[0] to it[4], // u-d
-          it[1] to it[5], // ur-dl
-          it[2] to it[6], // r-l
-          it[3] to it[7], // dr-ul
-        )
-          .map { pair ->
-            if (pair.first == null || pair.second == null) {
-              false
-            } else {
-              pair.first!! xor pair.second!!
-            }
-          }
-      }
-      .map { (it[0] and it[2]) to (it[1] and it[3]) }
-      .filter { it.first || it.second }
+        "${it.second[0].value}${it.first.value}${it.second[2].value}" to "${it.second[1].value}${it.first.value}${it.second[3].value}"
+      }.filter {
+        fun String.isMas() = "MAS" == this || "SAM" == this
 
-      .toList()
-
-    aas.forEach { println(it) }
-
-    return aas.count()
+        it.first.isMas() && it.second.isMas()
+      }.count()
   }
 
-//  println(silver(true))
-//  println(silver())
+  val k = read(false)
 
-  println(gold())
+  println(gold(k))
 }
