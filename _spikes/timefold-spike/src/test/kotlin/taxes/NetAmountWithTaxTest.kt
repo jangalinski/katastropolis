5f5b741200a71444976396b9021a81ecacf93851
Jan Galinski <jan.galinski@holisticon.de>
2025-11-06 18:13:25 +0100

    fix test

--- _spikes/timefold-spike/src/test/kotlin/taxes/NetAmountWithTaxTest.kt
+++ _spikes/timefold-spike/src/test/kotlin/taxes/NetAmountWithTaxTest.kt
@@ -3,14 +3,14 @@ package io.github.jangalinski.spike.timefold.taxes
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
-import java.math.MathContext
-import java.math.RoundingMode
-
 
 class NetAmountWithTaxTest {
+
   @Test
   fun `calculate tax for 0`() {
-    val amount = NetAmountWithTax(NetAmount(10.toBigDecimal()))
+    val amount = NetAmountWithTax(NetAmount(10.toBigDecimal())).apply {
+      taxRate = TaxRate.PCT_00
+    }
 
     assertThat(amount.netAmount.amount).isEqualTo(10.toBigDecimal())
     assertThat(amount.grossAmount.amount).isEqualTo(10.toBigDecimal())
@@ -19,8 +19,9 @@ class NetAmountWithTaxTest {
 
   @Test
   fun `calculate tax for 19`() {
-    val amount = NetAmountWithTax(NetAmount(100.toBigDecimal()))
-    amount.taxRate = TaxRate.PCT_19
+    val amount = NetAmountWithTax(NetAmount(100.toBigDecimal())).apply {
+      taxRate = TaxRate.PCT_19
+    }
 
     assertThat(amount.netAmount.amount).isEqualTo(100.toBigDecimal())
     assertThat(amount.grossAmount.rounded()).isEqualTo(GrossAmount(119.toBigDecimal()).rounded())
