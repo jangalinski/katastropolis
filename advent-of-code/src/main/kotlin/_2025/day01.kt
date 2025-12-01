fc0beea29d8ff5f8726764222f79f05afaf47418
Jan Galinski <jan.galinski@holisticon.de>
2025-12-01 23:58:33 +0100

    25/day1 tune

--- advent-of-code/src/main/kotlin/_2025/day01.kt
+++ advent-of-code/src/main/kotlin/_2025/day01.kt
@@ -1,13 +1,18 @@
 package io.github.jangalinski.kata.aoc._2025
 
-import arrow.core.raise.result
 import io.github.jangalinski.kata.aoc.AocUtil
 import io.github.jangalinski.kata.aoc.AocUtil.StringExt.nonEmptyLines
+import kotlin.math.absoluteValue
+import kotlin.math.sign
 
 fun main() {
 
-  fun read(test: Boolean) : List<Pair<String,Int>> = if (test) {
-    """
+  fun parse(input: String): List<Int> = input.trim().nonEmptyLines()
+    .map { if (it.first() == 'R') it.drop(1).toInt() else -it.drop(1).toInt() }
+
+  fun read(test: Boolean): List<Int> = parse(
+    if (test) {
+      """
       L68
       L30
       R48
@@ -19,40 +24,19 @@ fun main() {
       R14
       L82
     """.trimIndent()
-  } else {
-    AocUtil.Input(year = 2025, day = 1, part = 1, test).contentRaw
-  }.nonEmptyLines().map {
-    val (d,v) = it.first().toString() to it.drop(1).toInt()
-    d to v.toInt()
-  }
-
-  fun silver(test: Boolean = false) : Int = sequence {
-    read(test).fold(50) { acc, (d,v) ->
-      require(acc >= 0)
-      val s = acc +  (if (d == "R") v else -v)
-      val n = (if (s>=0) s else s+100) % 100
-      if (n == 0)
-        yield(n)
-      n
+    } else {
+      AocUtil.Input(year = 2025, day = 1, part = 1, test).contentRaw
     }
-  }.count()
+  )
 
+  fun silver(test: Boolean = false): Int = read(test).runningFold(50) { acc, v ->
+    (acc + v).mod(100)
+  }.count { it == 0 }
 
-  println(silver(false))
+  fun gold(test: Boolean = false): Int = read(test)
+    .flatMap { n -> IntArray(n.absoluteValue, { n.sign }).asIterable() }
+    .runningFold(50) { acc, v -> (acc + v).mod(100) }
+    .count { it == 0 }
 
-//  Following these rotations would cause the dial to move as follows:
-//
-//  The dial starts by pointing at 50.
-//  The dial is rotated L68 to point at 82.
-//  The dial is rotated L30 to point at 52.
-//  The dial is rotated R48 to point at 0.
-//  The dial is rotated L5 to point at 95.
-//  The dial is rotated R60 to point at 55.
-//  The dial is rotated L55 to point at 0.
-//  The dial is rotated L1 to point at 99.
-//  The dial is rotated L99 to point at 0.
-//  The dial is rotated R14 to point at 14.
-//  The dial is rotated L82 to point at 32.
-
-
+  println(gold(false))
 }
